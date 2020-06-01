5832eff051599cc32479a0a27c75632e9119224c
João Victor Almeida Costa <jvac65@gmail.com>
2020-06-01 11:36:38 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import api from "./services/api";
 
 function App() {
+  const [cep, setCep] = useState("");
+  const [logradouro, setLogradouro] = useState("");
+  const [bairro, setBairro] = useState("");
+  const [localidade, setLocalidade] = useState("");
+  const [uf, setUf] = useState("");
+
+  async function BuscarCEP() {
+    const response = await api.get(`${cep}/json`);
+    const result = response.data;
+    setLogradouro(result.logradouro);
+    setBairro(result.bairro);
+    setLocalidade(result.localidade);
+    setUf(result.uf);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container row">
+        <div className="input-group col-lg-12">
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Digite o CEP que deseja buscar..."
+            value={cep}
+            onChange={(e) => setCep(e.target.value)}
+          />
+          <div className="input-group-append">
+            <button className="btn btn-primary" onClick={() => BuscarCEP()}>
+              Buscar
+            </button>
+          </div>
+        </div>
+        <div className="col-lg-12">
+          <h5>Logradouro: {logradouro}</h5>
+          <h5>Bairro: {bairro}</h5>
+          <h5>Localidade: {localidade}</h5>
+          <h5>UF: {uf}</h5>
+        </div>
+      </div>
+    </>
   );
 }
 
